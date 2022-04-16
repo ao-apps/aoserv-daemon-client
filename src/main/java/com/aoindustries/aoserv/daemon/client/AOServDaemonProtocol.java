4f9d1d4668cc54faffef17eac7a5a03785859d34
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:00:23 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoindustries/aoserv/daemon/client/AOServDaemonProtocol.java
+++ src/main/java/com/aoindustries/aoserv/daemon/client/AOServDaemonProtocol.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with aoserv-daemon-client.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoindustries.aoserv.daemon.client;
 
 import com.aoindustries.aoserv.client.schema.AoservProtocol;
